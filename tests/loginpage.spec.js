2e6bdcffe649fadb05c740f67454d6a19d226c76
KavindaNishanJayaweera <kavindajayaweera115@gmail.com>
2026-06-04 20:48:22 +0530

    feat: remove hardocde values

--- tests/loginpage.spec.js
+++ tests/loginpage.spec.js
@@ -7,8 +7,8 @@ test('login', async function loginPage({page}){
     await page.goto('https://test-automation-app.vercel.app/login');
     //create a obj and assign it to Vari
     let login = new LoginPage(page);
-    await login.typeUsername();
-    await login.typePassword();
+    await login.typeUsername('test@gmail.com');
+    await login.typePassword('123');
     await login.clickBtn();
     //Previos - await expect(page.locator('[data-slot="alert-description"]')).toHaveText('Invalid email or password. Please try again.');
     //Now
@@ -18,8 +18,8 @@ test('login', async function loginPage({page}){
 test('loginerror',async function loginPageTestII({page}){
     await page.goto('https://test-automation-app.vercel.app/login');
     let login = new LoginPage(page);
-    await login.typeUsername();
-    await login.typePassword();
+    await login.typeUsername('demo@gmail.com');
+    await login.typePassword('Admin123');
     await login.clickBtn();
-
+    await expect(login.getErrorMessage()).toHaveText('Invalid email or password. Please try again.');
 })
