f32e410e7cc8d1ceea44345edccdf4dd6f5b2e51
KavindaNishanJayaweera <kavindajayaweera115@gmail.com>
2026-06-26 14:14:28 +0530

    feat: Naviigation

--- pages/NavigationPanel.js
+++ pages/NavigationPanel.js
@@ -4,12 +4,21 @@ export class NavigationPage{
     constructor(page){
 
         this.btnaddstudent = page.locator('#btnaddStudent');
-        //this.btnmanagestu = page.locator('#btnmanageStudent');
+        this.btnmanagestu = page.locator('#btnmanageStudent');
+        this.btnPayments = page.locator('#btnpayments');
 
     }
 
     returnaddStudentBtn(){
         return this.btnaddstudent;
+    }
+
+    returnManageStudentbtn(){
+        return this.btnmanagestu;
+    }
+
+     returnpaymentbtn(){
+        return this.btnPayments;
     }
 
 }
